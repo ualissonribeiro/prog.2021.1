7faf0192dabe8758d8bc185ddfd8b8d1ab63656c
ualissonribeiro <88834471+ualissonribeiro@users.noreply.github.com>
2021-10-09 22:52:06 -0300

    atividade rATO
    ATV

---
+++ atividade aula 4/atividade rato.py
@@ -0,0 +1,10 @@
+print ( "o animal está habituado?")
+animalhabituado=input()
+if animalhabituado=="sim":
+    print ("avançar para o regime de aproximações sucessivas ")
+else:
+    print ("habituar animal ao experimento")
+
+if animalhabituado=="sim":
+    print("O rato diminuiu a aproximação?")
+    
